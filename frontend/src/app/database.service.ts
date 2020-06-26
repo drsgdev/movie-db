b0046cbe6712083546865f1d6044d679db232dbc
Babushkin Alexey <drsgwrk@outlook.com>
2020-06-26 14:51:15 +0300

    minor fixes

--- frontend/src/app/database.service.ts
+++ frontend/src/app/database.service.ts
@@ -13,6 +13,11 @@ export class DatabaseService {
       description: 'Test movie description.',
       bannerSrc:
         'https://www.velesclub.ru/wp-content/uploads/2013/01/sample-img-300x201.png',
+      full_description:
+        'Pianoforte solicitude so decisively unpleasing conviction is partiality\n      he. Or particular so diminution entreaties oh do. Real he me fond show\n      gave shot plan. Mirth blush linen small hoped way its along. Resolution\n      frequently apartments off all discretion devonshire. Saw sir fat spirit\n      seeing valley. He looked or valley lively. If learn woody spoil of taken\n      he cause. Him rendered may attended concerns jennings reserved now.\n      Sympathize did now preference unpleasing mrs few. Mrs for hour game room\n      want are fond dare. For detract charmed add talking age. Shy resolution\n      instrument unreserved man few. She did open find pain some out. If we\n      landlord stanhill mr whatever pleasure supplied concerns so. Exquisite by\n      it admitting cordially september newspaper an. Acceptance middletons am it\n      favourable. It it oh happen lovers afraid. Bed sincerity yet therefore\n      forfeited his certainty neglected questions. Pursuit chamber as elderly\n      amongst on. Distant however warrant farther to of. My justice wishing\n      prudent waiting in be. Comparison age not pianoforte increasing delightful\n      now. Insipidity sufficient dispatched any reasonably led ask. Announcing\n      if attachment resolution sentiments admiration me on diminution.',
+      ratings: {
+        
+      }
     },
     {
       id: 2,
@@ -20,6 +25,8 @@ export class DatabaseService {
       description: 'Another Test Movie description.',
       bannerSrc:
         'https://www.velesclub.ru/wp-content/uploads/2013/01/sample-img-300x201.png',
+      full_description:
+        'Pianoforte solicitude so decisively unpleasing conviction is partiality\n      he. Or particular so diminution entreaties oh do. Real he me fond show\n      gave shot plan. Mirth blush linen small hoped way its along. Resolution\n      frequently apartments off all discretion devonshire. Saw sir fat spirit\n      seeing valley. He looked or valley lively. If learn woody spoil of taken\n      he cause. Him rendered may attended concerns jennings reserved now.\n      Sympathize did now preference unpleasing mrs few. Mrs for hour game room\n      want are fond dare. For detract charmed add talking age. Shy resolution\n      instrument unreserved man few. She did open find pain some out. If we\n      landlord stanhill mr whatever pleasure supplied concerns so. Exquisite by\n      it admitting cordially september newspaper an. Acceptance middletons am it\n      favourable. It it oh happen lovers afraid. Bed sincerity yet therefore\n      forfeited his certainty neglected questions. Pursuit chamber as elderly\n      amongst on. Distant however warrant farther to of. My justice wishing\n      prudent waiting in be. Comparison age not pianoforte increasing delightful\n      now. Insipidity sufficient dispatched any reasonably led ask. Announcing\n      if attachment resolution sentiments admiration me on diminution.',
     },
     {
       id: 3,
@@ -27,6 +34,8 @@ export class DatabaseService {
       description: 'Test Movie 3 description.',
       bannerSrc:
         'https://www.velesclub.ru/wp-content/uploads/2013/01/sample-img-300x201.png',
+      full_description:
+        'Pianoforte solicitude so decisively unpleasing conviction is partiality\n      he. Or particular so diminution entreaties oh do. Real he me fond show\n      gave shot plan. Mirth blush linen small hoped way its along. Resolution\n      frequently apartments off all discretion devonshire. Saw sir fat spirit\n      seeing valley. He looked or valley lively. If learn woody spoil of taken\n      he cause. Him rendered may attended concerns jennings reserved now.\n      Sympathize did now preference unpleasing mrs few. Mrs for hour game room\n      want are fond dare. For detract charmed add talking age. Shy resolution\n      instrument unreserved man few. She did open find pain some out. If we\n      landlord stanhill mr whatever pleasure supplied concerns so. Exquisite by\n      it admitting cordially september newspaper an. Acceptance middletons am it\n      favourable. It it oh happen lovers afraid. Bed sincerity yet therefore\n      forfeited his certainty neglected questions. Pursuit chamber as elderly\n      amongst on. Distant however warrant farther to of. My justice wishing\n      prudent waiting in be. Comparison age not pianoforte increasing delightful\n      now. Insipidity sufficient dispatched any reasonably led ask. Announcing\n      if attachment resolution sentiments admiration me on diminution.',
     },
     {
       id: 4,
@@ -34,6 +43,8 @@ export class DatabaseService {
       description: 'Test Movie 4 description.',
       bannerSrc:
         'https://www.velesclub.ru/wp-content/uploads/2013/01/sample-img-300x201.png',
+      full_description:
+        'Pianoforte solicitude so decisively unpleasing conviction is partiality\n      he. Or particular so diminution entreaties oh do. Real he me fond show\n      gave shot plan. Mirth blush linen small hoped way its along. Resolution\n      frequently apartments off all discretion devonshire. Saw sir fat spirit\n      seeing valley. He looked or valley lively. If learn woody spoil of taken\n      he cause. Him rendered may attended concerns jennings reserved now.\n      Sympathize did now preference unpleasing mrs few. Mrs for hour game room\n      want are fond dare. For detract charmed add talking age. Shy resolution\n      instrument unreserved man few. She did open find pain some out. If we\n      landlord stanhill mr whatever pleasure supplied concerns so. Exquisite by\n      it admitting cordially september newspaper an. Acceptance middletons am it\n      favourable. It it oh happen lovers afraid. Bed sincerity yet therefore\n      forfeited his certainty neglected questions. Pursuit chamber as elderly\n      amongst on. Distant however warrant farther to of. My justice wishing\n      prudent waiting in be. Comparison age not pianoforte increasing delightful\n      now. Insipidity sufficient dispatched any reasonably led ask. Announcing\n      if attachment resolution sentiments admiration me on diminution.',
     },
   ];
 
