dbd771c92c7d6f4d7f77f02d2322b5c31ce85540
Izomorf <maxim.p.kochukov@gmail.com>
2020-07-07 19:36:31 +0300

    configured maven structure to build both front+back

--- backend/src/main/java/com/github/drsgdev/config/WebConfig.java
+++ backend/src/main/java/com/github/drsgdev/config/WebConfig.java
@@ -3,10 +3,11 @@ package com.github.drsgdev.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
@@ -14,5 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").maxAge(3600l).allowedHeaders("*")
         .allowCredentials(true);
     WebMvcConfigurer.super.addCorsMappings(registry);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/resources/**")
+            .addResourceLocations("/public", "classpath:/static/")
+            .setCachePeriod(31556926);
   }
 }
